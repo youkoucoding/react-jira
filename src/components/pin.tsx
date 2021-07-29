73845df467277bf3f28749f4687a296ebfedf27a
youkoucoding <youkousai@gmail.com>
2021-07-29 23:48:31 +0900

    Add useEditProject to edit the pin

---
+++ src/components/pin.tsx
@@ -0,0 +1,19 @@
+import { Rate } from "antd";
+import React from "react";
+
+interface PinProps extends React.ComponentProps<typeof Rate> {
+  checked: Boolean;
+  onCheckedChange?: (checked: boolean) => void;
+}
+
+export const Pin = ({ checked, onCheckedChange, ...restProps }: PinProps) => {
+  return (
+    <Rate
+      count={1}
+      value={checked ? 1 : 0}
+      // onChange={ num=>onCheckedChange?.(Boolean(num))}
+      onChange={(num) => onCheckedChange?.(!!num)}
+      {...restProps}
+    />
+  );
+};
--- src/utils/project.ts
+++ src/utils/project.ts
@@ -22,3 +22,33 @@ export const useProjects = (param: Partial<Project>) => {
 
   return result;
 };
+
+export const useEditProject = () => {
+  const { run, ...asyncResult } = useAsync();
+  const client = useHttp();
+  const mutate = (params: Partial<Project>) => {
+    run(
+      client(`projects/${params.id}`, {
+        data: params,
+        method: "PATCH",
+      })
+    );
+  };
+
+  return { mutate, ...asyncResult };
+};
+
+export const useAddProject = () => {
+  const { run, ...asyncResult } = useAsync();
+  const client = useHttp();
+  const mutate = (params: Partial<Project>) => {
+    run(
+      client(`projects/${params.id}`, {
+        data: params,
+        method: "POST",
+      })
+    );
+  };
+
+  return { mutate, ...asyncResult };
+};
