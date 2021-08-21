344e339b32b6f654921b9652fe921c469bd0a359
youkoucoding <youkousai@gmail.com>
2021-08-21 23:08:38 +0900

    Add kanban's isLoading-spin

--- src/screens/kanban/index.tsx
+++ src/screens/kanban/index.tsx
@@ -1,27 +1,41 @@
 import styled from "@emotion/styled";
+import { Spin } from "antd";
 import { ScreenContainer } from "components/lib";
 import React from "react";
 import { useDocumentTitle } from "utils";
 import { useKanbans } from "utils/kanban";
+import { useTasks } from "utils/task";
 import { KanbanColumn } from "./kanban-column";
 import { SearchPanel } from "./search-panel";
-import { useKanbanSearchParams, useProjectInUrl } from "./util";
+import {
+  useKanbanSearchParams,
+  useProjectInUrl,
+  useTasksSearchParams,
+} from "./util";
 
 export const KanbanScreen = () => {
   useDocumentTitle("かんばんList");
 
   const { data: currentProject } = useProjectInUrl();
-  const { data: kanbans } = useKanbans(useKanbanSearchParams());
+  const { data: kanbans, isLoading: kanbanIsLoading } = useKanbans(
+    useKanbanSearchParams()
+  );
+  const { isLoading: taskIsLoading } = useTasks(useTasksSearchParams());
+  const isLoading = kanbanIsLoading || taskIsLoading;
 
   return (
     <ScreenContainer>
       <h1>{currentProject?.name}看板</h1>
       <SearchPanel />
-      <ColumnsContainer>
-        {kanbans?.map((kanban) => (
-          <KanbanColumn key={kanban.id} kanban={kanban} />
-        ))}
-      </ColumnsContainer>
+      {isLoading ? (
+        <Spin size={"large"} />
+      ) : (
+        <ColumnsContainer>
+          {kanbans?.map((kanban) => (
+            <KanbanColumn key={kanban.id} kanban={kanban} />
+          ))}
+        </ColumnsContainer>
+      )}
     </ScreenContainer>
   );
 };
